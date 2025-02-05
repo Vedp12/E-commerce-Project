5cd081703f03db97f135fc81fef0c303304daef4
Ved <ved00684@gmail.com>
2025-02-05 16:11:03 +0530

    images added

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path 
 from myapp import views
 urlpatterns = [
+    # path('admin/', admin.site.urls),
     path('',views.home,name='home'),
     path('cart/',views.cart,name='cart'),
     path('about',views.about,name='about'),
